a1b63eaebbb4bea1d14ca7269fc1022e48bf79d7
Docheinstein <dottorestefano97@gmail.com>
2024-01-21 17:58:45 +0100

    Add PPU tests for window+sprite timing and win_tile_map change

---
+++ source/ppu/pixel_transfer_timing_window_sprite_wx7_x8_scx0_round1.asm
@@ -0,0 +1,73 @@
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+INCLUDE "dma.inc"
+
+; Check the duration of Pixel Transfer (Mode 3) with window enabled
+; and sprites overlapping with the window.
+
+EntryPoint:
+    ; Disable PPU
+    DisablePPU
+
+    ; Copy OAM data
+    ResetOAM
+    Memcpy $fe00, OamData, OamDataEnd - OamData
+
+    ; Set WX
+    ld a, 7
+    ldh [rWX], a
+
+    ; Set WY
+    ld a, 8
+    ldh [rWY], a
+
+    ; Enable PPU with window on
+    ld a, LCDCF_ON | LCDCF_BGON | LCDCF_WINON | LCDCF_WIN9C00 | LCDCF_OBJON
+    ldh [rLCDC], a
+
+    ; Wait line 7
+    WaitScanline 7
+
+    ; Go out of OAM scan
+    Nops 20
+
+    ; Reset interrupts
+    xor a
+    ldh [rIF], a
+
+    ; Enable STAT interrupt
+    ld a, IEF_STAT
+    ldh [rIE], a
+
+    ; Enable OAM interrupt
+    ld a, STATF_MODE10
+    ldh [rSTAT], a
+
+    ; Enable OAM interrupt
+    ei
+
+    Nops 114
+
+    jp TestFail
+
+TestStart:
+    ; Wait until end of Pixel Transfer
+    Nops 56
+
+    ldh a, [rSTAT]
+    ld b, a
+    ld a, $a3
+    cp b
+
+    jp nz, TestFail
+
+    jp TestSuccess
+
+
+OamData:
+db 24, 8, 0, 0
+OamDataEnd:
+
+SECTION "STAT handler", ROM0[$48]
+    jp TestStart
+
