0f6773d5aad41a3a7ed6ac4872a4319a276843fe
Docheinstein <dottorestefano97@gmail.com>
2024-04-13 12:52:50 +0200

    Add interrupts IME timing test

---
+++ source/interrupts/ei_timing.asm
@@ -0,0 +1,32 @@
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+
+; Check how much it takes for EI to have effect.
+
+EntryPoint:
+    ; Manually set interrupt in IE and IF
+    ld a, 1
+    ldh [rIE], a
+    ldh [rIF], a
+
+    xor a
+
+    ; Enable interrupt: but it should have effect after the following instruction
+    ei
+
+    ; Reset IF: interrupt should not take place
+    ldh [rIF], a
+
+    ; Just ensure that interrupt does not happen.
+    nop
+
+    ; If we get here, EI timing is right and interrupt didn't take place
+    jp TestSuccess
+
+SECTION "Zero Handler", ROM0[$00]
+    ; We shouldn't reach this point.
+    jp TestFail
+
+SECTION "VBlank handler", ROM0[$40]
+    ; We shouldn't reach this point.
+    jp TestFail
