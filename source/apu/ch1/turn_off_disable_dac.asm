97834c81fcbe76ecb9faa51a2ad9a00f3daa305e
Docheinstein <dottorestefano97@gmail.com>
2024-09-19 16:25:55 +0200

    Add APU CH3 tests

---
+++ source/apu/ch1/turn_off_disable_dac.asm
@@ -0,0 +1,35 @@
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+INCLUDE "apu.inc"
+INCLUDE "print.inc"
+INCLUDE "debug.inc"
+
+; Disabling PPU reset NR12 and turns off CH1 DAC
+
+EntryPoint:
+    EnableAPU
+
+    ; DAC = 1
+    ld a, $F8
+    ldh [rNR12], a
+
+    DisableAPU
+
+    ; NR12 should be reset
+    ldh a, [rNR12]
+
+    cp $00
+    jp nz, TestFail
+
+    EnableAPU
+
+    ; Trigger = 1
+    ld a, $80
+    ldh [rNR14], a
+
+    ; CH1 should be off
+    ldh a, [rNR52]
+    and $01
+    jp nz, TestFail
+
+    jp TestSuccess
---
+++ source/apu/ch3/write_wave_period5_delay4.asm
@@ -0,0 +1,50 @@
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+INCLUDE "apu.inc"
+
+; Writing to Wave RAM while it is on either writes the current byte CH3 is accessing or it is ignored.
+
+EntryPoint:
+    EnableAPU
+
+    Memcpy $FF30, WaveRam, WaveRamEnd - WaveRam
+
+    ; Enable = 1
+    ld a, $80
+    ldh [rNR30], a
+
+    ; Volume = 0
+    ld a, $00
+    ldh [rNR32], a
+
+    ; Period = 5
+    ld a, $FB
+    ldh [rNR33], a
+
+    ld a, $87
+    ldh [rNR34], a
+
+    Nops 4
+
+    ; Write to wave ream
+    ldh [$FF30], a
+
+    ; Enable = 0
+    xor a
+    ldh [rNR30], a
+
+    ; Read wave ram
+    Memcmp $ff30, ExpectedWaveRam, ExpectedWaveRamEnd - ExpectedWaveRam
+    jp nz, TestFail
+
+    jp TestSuccess
+
+WaveRam:
+db $00,$11,$22,$33,$44,$55,$66,$77
+db $88,$99,$AA,$BB,$CC,$DD,$EE,$FF
+WaveRamEnd:
+
+ExpectedWaveRam:
+db $00,$11,$22,$33,$44,$55,$66,$77
+db $88,$99,$AA,$BB,$CC,$DD,$EE,$FF
+ExpectedWaveRamEnd:
