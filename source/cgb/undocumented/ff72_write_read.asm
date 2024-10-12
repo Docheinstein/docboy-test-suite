b9d9280d58e47e805f7274738e651a80f86ec5c6
Docheinstein <dottorestefano97@gmail.com>
2024-10-13 01:34:57 +0200

    Add undocumented registers tests

--- source/cgb/undocumented/ff72_write_read.asm
+++ source/cgb/undocumented/ff72_write_read.asm
@@ -2,21 +2,21 @@ INCLUDE "hardware.inc"
 INCLUDE "common.inc"
 INCLUDE "cgb.inc"
 
-; Check write/read behavior of RP register.
+; Check write/read behavior of FF72 register.
 
 EntryPoint:
-    ; Write 00 -> Read 3E
+    ; Write 00 -> Read 00
     ld a, $00
-    ldh [rRP], a
-    ldh a, [rRP]
+    ldh [$FF72], a
+    ldh a, [$FF72]
 
-    cp $3e
+    cp $00
     jp nz, TestFailCGB
 
     ; Write FF -> Read FF
     ld a, $ff
-    ldh [rRP], a
-    ldh a, [rRP]
+    ldh [$FF72], a
+    ldh a, [$FF72]
 
     cp $ff
     jp nz, TestFailCGB
--- source/cgb/undocumented/ff75_write_read.asm
+++ source/cgb/undocumented/ff75_write_read.asm
@@ -2,21 +2,21 @@ INCLUDE "hardware.inc"
 INCLUDE "common.inc"
 INCLUDE "cgb.inc"
 
-; Check write/read behavior of RP register.
+; Check write/read behavior of FF75 register.
 
 EntryPoint:
-    ; Write 00 -> Read 3E
+    ; Write 00 -> Read 8F
     ld a, $00
-    ldh [rRP], a
-    ldh a, [rRP]
+    ldh [$FF75], a
+    ldh a, [$FF75]
 
-    cp $3e
+    cp $8F
     jp nz, TestFailCGB
 
     ; Write FF -> Read FF
     ld a, $ff
-    ldh [rRP], a
-    ldh a, [rRP]
+    ldh [$FF75], a
+    ldh a, [$FF75]
 
     cp $ff
     jp nz, TestFailCGB
