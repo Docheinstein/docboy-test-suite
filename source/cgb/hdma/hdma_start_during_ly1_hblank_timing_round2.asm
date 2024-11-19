97f3a19de2ffa6db92d18ac6cd8dadbad13ffc34
Docheinstein <dottorestefano97@gmail.com>
2024-11-20 00:33:47 +0100

    Add HDMA tests for HDMA started during HBlank

---
+++ source/cgb/hdma/hdma_start_during_ly1_hblank_timing_round2.asm
@@ -0,0 +1,62 @@
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+INCLUDE "cgb.inc"
+
+; Start a HDMA (HBlank) transfer during HBlank.
+; Check the timing of the transfer.
+
+EntryPoint:
+    DisablePPU
+
+    ; Set VRAM data
+    Memset $8000, $ab, $80
+
+    ; Copy data to WRAM2
+    Memcpy $D000, VramData, VramDataEnd - VramData
+
+    ; Source address = D000
+    ld a, $D0
+    ldh [rHDMA1], a
+
+    ld a, $00
+    ldh [rHDMA2], a
+
+    ; Dest address = 8000
+    ld a, $80
+    ldh [rHDMA3], a
+
+    ld a, $00
+    ldh [rHDMA4], a
+
+    ; Enable PPU again
+    EnablePPU
+
+    ; Wait for HBLank of LY=1
+    Nops 60 + 114
+
+    ; Bit 7 = 1 (HBlank)
+    ; Length = 64 bytes / $10 - 1 => 3
+    ld a, $83
+    ldh [rHDMA5], a
+
+    ; --- transfer happens here ---
+
+    Nops 312
+
+    ldh a, [rHDMA5]
+
+    cp $ff
+    jp nz, TestFailCGB
+
+    jp TestSuccessCGB
+
+VramData:
+    db $00, $11, $22, $33, $44, $55, $66, $77
+    db $88, $99, $aa, $bb, $cc, $dd, $ee, $ff
+    db $11, $22, $33, $44, $55, $66, $77, $88
+    db $99, $aa, $bb, $cc, $dd, $ee, $ff, $00
+    db $22, $33, $44, $55, $66, $77, $88, $99
+    db $aa, $bb, $cc, $dd, $ee, $ff, $00, $11
+    db $33, $44, $55, $66, $77, $88, $99, $aa
+    db $bb, $cc, $dd, $ee, $ff, $00, $11, $22
+VramDataEnd:
