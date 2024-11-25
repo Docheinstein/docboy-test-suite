9a8170efbc0c042ebcb71eb8391a5f605a84565a
Docheinstein <dottorestefano97@gmail.com>
2024-11-26 00:22:41 +0100

    Add HDMA test for source overflow

---
+++ source/cgb/hdma/gdma_overflow_source.asm
@@ -0,0 +1,81 @@
+;! MBC_TYPE=2
+;! RAM_SIZE=3
+
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+INCLUDE "cgb.inc"
+
+; Perform a HDMA (General Purpose) transfer using Cartridge RAM as source.
+; Restart the transfer without resetting source address.
+; After 32 transfers, the source should return the same.
+
+EntryPoint:
+    DisablePPU
+
+    ; Enable Cartridge RAM
+    ld hl, $0000
+    ld a, $0A
+    ld [hl], a
+
+    ; Set Cartridge RAM data
+    Memset $a000, $ab, $80
+    Memcpy $a000, Data, DataEnd - Data
+
+    ; Source address = a000
+    ld a, $a0
+    ldh [rHDMA1], a
+
+    ld a, $00
+    ldh [rHDMA2], a
+
+REPT 33
+    Memset $8000, $cd, $80
+
+    ; Dest address = 8000
+    ld a, $80
+    ldh [rHDMA3], a
+
+    ld a, $00
+    ldh [rHDMA4], a
+
+    ; Bit 7 = 0 (general purpose)
+    ; Length = 2048 bytes
+    ld a, $7f
+    ldh [rHDMA5], a
+ENDR
+    ; --- transfer happens here ---
+
+    Memcmp $8000, ExpectedVramData, ExpectedVramDataEnd - ExpectedVramData
+    jp nz, TestFailCGB
+
+    jp TestSuccessCGB
+
+Data:
+    db $00, $11, $22, $33, $44, $55, $66, $77
+    db $88, $99, $aa, $bb, $cc, $dd, $ee, $ff
+    db $11, $22, $33, $44, $55, $66, $77, $88
+    db $99, $aa, $bb, $cc, $dd, $ee, $ff, $00
+    db $22, $33, $44, $55, $66, $77, $88, $99
+    db $aa, $bb, $cc, $dd, $ee, $ff, $00, $11
+    db $33, $44, $55, $66, $77, $88, $99, $aa
+    db $bb, $cc, $dd, $ee, $ff, $00, $11, $22
+DataEnd:
+
+ExpectedVramData:
+    db $00, $11, $22, $33, $44, $55, $66, $77
+    db $88, $99, $aa, $bb, $cc, $dd, $ee, $ff
+    db $11, $22, $33, $44, $55, $66, $77, $88
+    db $99, $aa, $bb, $cc, $dd, $ee, $ff, $00
+    db $22, $33, $44, $55, $66, $77, $88, $99
+    db $aa, $bb, $cc, $dd, $ee, $ff, $00, $11
+    db $33, $44, $55, $66, $77, $88, $99, $aa
+    db $bb, $cc, $dd, $ee, $ff, $00, $11, $22
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+ExpectedVramDataEnd:
