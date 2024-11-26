cf5ee735a0a24292bf16eb1d2cf9f420f05d9816
Docheinstein <dottorestefano97@gmail.com>
2024-11-26 01:11:03 +0100

    Add HDMA tests for source/destination overflow/rollback

---
+++ source/cgb/hdma/gdma_overflow_destination_8800_rollback.asm
@@ -0,0 +1,78 @@
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+INCLUDE "cgb.inc"
+
+; Perform a HDMA (General Purpose) transfer using $8800 as destination.
+; Restart the transfer without resetting destination address.
+; After 3 transfer, the source should be $8000.
+
+EntryPoint:
+    DisablePPU
+
+    ; Enable Cartridge RAM
+    ld hl, $0000
+    ld a, $0A
+    ld [hl], a
+
+    ; Set WRAM data
+    Memset $C000, $ab, $80
+    Memcpy $C000, Data, DataEnd - Data
+
+    ; Dest address = 8800
+    ld a, $88
+    ldh [rHDMA3], a
+
+    ld a, $00
+    ldh [rHDMA4], a
+
+REPT 4
+    Memset $8000, $cd, $80
+
+    ; Source address = c000
+    ld a, $c0
+    ldh [rHDMA1], a
+
+    ld a, $00
+    ldh [rHDMA2], a
+
+    ; Bit 7 = 0 (general purpose)
+    ; Length = 2048 bytes
+    ld a, $7f
+    ldh [rHDMA5], a
+ENDR
+    ; --- transfer happens here ---
+
+    Memcmp $8000, ExpectedVramData, ExpectedVramDataEnd - ExpectedVramData
+    jp nz, TestFailCGB
+
+    jp TestSuccessCGB
+
+Data:
+    db $00, $11, $22, $33, $44, $55, $66, $77
+    db $88, $99, $aa, $bb, $cc, $dd, $ee, $ff
+    db $11, $22, $33, $44, $55, $66, $77, $88
+    db $99, $aa, $bb, $cc, $dd, $ee, $ff, $00
+    db $22, $33, $44, $55, $66, $77, $88, $99
+    db $aa, $bb, $cc, $dd, $ee, $ff, $00, $11
+    db $33, $44, $55, $66, $77, $88, $99, $aa
+    db $bb, $cc, $dd, $ee, $ff, $00, $11, $22
+DataEnd:
+
+ExpectedVramData:
+    db $00, $11, $22, $33, $44, $55, $66, $77
+    db $88, $99, $aa, $bb, $cc, $dd, $ee, $ff
+    db $11, $22, $33, $44, $55, $66, $77, $88
+    db $99, $aa, $bb, $cc, $dd, $ee, $ff, $00
+    db $22, $33, $44, $55, $66, $77, $88, $99
+    db $aa, $bb, $cc, $dd, $ee, $ff, $00, $11
+    db $33, $44, $55, $66, $77, $88, $99, $aa
+    db $bb, $cc, $dd, $ee, $ff, $00, $11, $22
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+ExpectedVramDataEnd:
---
+++ source/cgb/hdma/gdma_overflow_source_illegal.asm
@@ -0,0 +1,83 @@
+;! MBC_TYPE=2
+;! RAM_SIZE=3
+
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+INCLUDE "cgb.inc"
+
+; Perform a HDMA (General Purpose) transfer using Cartridge RAM as source.
+; Restart the transfer without resetting source address.
+; After 8 transfer, the source should be in an illegal area (Echo RAM).
+
+EntryPoint:
+    DisablePPU
+
+    ; Enable Cartridge RAM
+    ld hl, $0000
+    ld a, $0A
+    ld [hl], a
+
+    ; Set Cartridge RAM data
+    Memset $a000, $ab, $80
+    Memcpy $a000, Data, DataEnd - Data
+
+    ; Source address = a000
+    ld a, $a0
+    ldh [rHDMA1], a
+
+    ld a, $00
+    ldh [rHDMA2], a
+
+REPT 9
+    Memset $8000, $cd, $80
+
+    ; Dest address = 8000
+    ld a, $80
+    ldh [rHDMA3], a
+
+    ld a, $00
+    ldh [rHDMA4], a
+
+    ; Bit 7 = 0 (general purpose)
+    ; Length = 2048 bytes
+    ld a, $7f
+    ldh [rHDMA5], a
+ENDR
+    ; --- transfer happens here ---
+
+    ; The transferred data should have a corrupted
+    ; byte at the beginning of the first chunk.
+    ld hl, $8000
+    ld a, [hl]
+    cp $ff
+    jp z, TestFailCGB
+
+    Memcmp $8001, ExpectedVramData, ExpectedVramDataEnd - ExpectedVramData
+    jp nz, TestFailCGB
+
+
+    jp TestSuccessCGB
+
+Data:
+    db $00, $11, $22, $33, $44, $55, $66, $77
+    db $88, $99, $aa, $bb, $cc, $dd, $ee, $ff
+    db $11, $22, $33, $44, $55, $66, $77, $88
+    db $99, $aa, $bb, $cc, $dd, $ee, $ff, $00
+    db $22, $33, $44, $55, $66, $77, $88, $99
+    db $aa, $bb, $cc, $dd, $ee, $ff, $00, $11
+    db $33, $44, $55, $66, $77, $88, $99, $aa
+    db $bb, $cc, $dd, $ee, $ff, $00, $11, $22
+DataEnd:
+
+ExpectedVramData:
+    db      $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+ExpectedVramDataEnd:
