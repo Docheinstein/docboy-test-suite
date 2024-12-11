088e735e1e953d7be610a1f62ac09f448897c0f6
Docheinstein <dottorestefano97@gmail.com>
2024-12-11 17:45:27 +0100

    Add CGB tests for precise Not Usable area ranges

---
+++ source/cgb/memory/not_usable_area_write_fea0_16_bytes.asm
@@ -0,0 +1,39 @@
+INCLUDE "hardware.inc"
+INCLUDE "common.inc"
+INCLUDE "cgb.inc"
+
+; Check how Not Usable area behaves when writing and reading back something.
+
+EntryPoint:
+    DisablePPU
+
+    Memset $fea0, $00, 96
+
+    ; Copy some data to FEA0
+    Memcpy $fea0, Data, DataEnd - Data
+
+    ; Read entire area back
+    Memcmp $fea0, ExpectedData, ExpectedDataEnd - ExpectedData
+
+    jp nz, TestFailCGB
+    jp TestSuccessCGB
+
+Data:
+    db $00, $01, $02, $03, $04, $05, $06, $07
+    db $08, $09, $0a, $0b, $0c, $0d, $0e, $0f
+DataEnd:
+
+ExpectedData:
+    db $00, $01, $02, $03, $04, $05, $06, $07
+    db $08, $09, $0a, $0b, $0c, $0d, $0e, $0f
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+    db $00, $00, $00, $00, $00, $00, $00, $00
+ExpectedDataEnd:
