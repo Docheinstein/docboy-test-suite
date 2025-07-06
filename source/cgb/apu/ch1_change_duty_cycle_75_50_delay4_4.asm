644e09833ac02daa13218d67d6190d4e34d90d9f
Docheinstein <dottorestefano97@gmail.com>
2025-07-07 00:56:13 +0200

    Add tests for APU CH1/CH2 duty cycle

---
+++ source/cgb/apu/ch1_change_duty_cycle_75_50_delay4_4.asm
@@ -0,0 +1,43 @@
+INCLUDE "all.inc"
+
+; Change the duty cycle from 75% to 50% with various delays and check the output.
+
+EntryPoint:
+    xor a
+    ldh [rDIV], a
+
+    DisableAPU
+    EnableAPU
+
+    xor a
+    ldh [rNR10], a
+
+    ; Duty Cycle = 75%
+    ld a, $C0
+    ldh [rNR11], a
+
+    ; Initial volume = F
+    ld a, $F0
+    ldh [rNR12], a
+
+    ; Period = 1
+    ld a, $FF
+    ldh [rNR13], a
+
+    ; Trigger = 1
+    ld a, $87
+    ldh [rNR14], a
+
+    Nops 4
+
+    ; Duty Cycle = 50%
+    ld a, $80
+    ldh [rNR11], a
+
+    Nops 4
+
+    ldh a, [rPCM12]
+    cp $0f
+
+    jp nz, TestFail
+    jp TestSuccess
---
+++ source/cgb/apu/ch1_change_duty_cycle_period_75_50_delay18_0.asm
@@ -0,0 +1,41 @@
+INCLUDE "all.inc"
+
+; Change the duty cycle from 75% to 50% with various delays and check the output.
+
+EntryPoint:
+    xor a
+    ldh [rDIV], a
+
+    DisableAPU
+    EnableAPU
+
+    xor a
+    ldh [rNR10], a
+
+    ; Duty Cycle = 75%
+    ld a, $C0
+    ldh [rNR11], a
+
+    ; Initial volume = F
+    ld a, $F0
+    ldh [rNR12], a
+
+    ; Period = 8
+    ld a, $F8
+    ldh [rNR13], a
+
+    ; Trigger = 1
+    ld a, $87
+    ldh [rNR14], a
+
+    Nops 18
+
+    ; Duty Cycle = 50%
+    ld a, $80
+    ldh [rNR11], a
+
+    ldh a, [rPCM12]
+    cp $00
+
+    jp nz, TestFail
+    jp TestSuccess
