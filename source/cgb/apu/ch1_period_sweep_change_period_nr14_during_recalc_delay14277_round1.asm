76210f68f8916f7bcaf0423bd339b261724a75b0
Docheinstein <dottorestefano97@gmail.com>
2025-08-31 19:19:29 +0200

    Add period sweep tests verifying NR14 write at the same time of period sweep tick

---
+++ source/cgb/apu/ch1_period_sweep_change_period_nr14_during_recalc_delay14277_round1.asm
@@ -0,0 +1,48 @@
+INCLUDE "all.inc"
+
+; Check what happens if period is changed with a retrigger at the same time recalculation happens.
+
+EntryPoint:
+    xor a
+    ldh [rDIV], a
+
+    DisableAPU
+    EnableAPU
+
+    ; Pace = 1
+    ; Direction = 0 (increase)
+    ; Step = 5
+    ld a, $15
+    ldh [rNR10], a
+
+    ; Duty Cycle = 75%
+    ld a, $C0
+    ldh [rNR11], a
+
+    ; Initial volume = A
+    ld a, $A0
+    ldh [rNR12], a
+
+    ; Period = 1280
+    ld a, $00
+    ldh [rNR13], a
+
+    ; Trigger = 1
+    ld a, $83
+    ldh [rNR14], a
+
+    Wait 16384
+    Wait 16384
+    Wait 14277
+
+    ; Trigger = 1
+    ld a, $87
+    ldh [rNR14], a
+
+    Wait 8203
+
+    ldh a, [rNR52]
+    cp $f1
+
+    jp nz, TestFail
+    jp TestSuccess
---
+++ source/cgb/double_speed/apu/double_speed_apu_ch1_period_sweep_change_period_nr14_during_recalc_step1_delay12241_shutdown_round2.asm
@@ -0,0 +1,62 @@
+INCLUDE "all.inc"
+
+; Check what happens if period is changed with a retrigger at the same time recalculation happens.
+
+EntryPoint:
+    ld hl, rNR14
+
+    xor a
+    ldh [rDIV], a
+
+    DisableAPU
+
+    ; Prepare speed switch
+    ld a, $01
+    ldh [rKEY1], a
+
+    ; Switch to double speed
+    stop
+
+    EnableAPU
+
+    ; Pace = 1
+    ; Direction = 0 (increase)
+    ; Step = 1
+    ld a, $11
+    ldh [rNR10], a
+
+    ; Duty Cycle = 75%
+    ld a, $C0
+    ldh [rNR11], a
+
+    ; Initial volume = A
+    ld a, $A0
+    ldh [rNR12], a
+
+    ; Period = 1280
+    ld a, $00
+    ldh [rNR13], a
+
+    ; Trigger = 1
+    ld a, $83
+    ldh [rNR14], a
+
+    Wait 16384
+    Wait 16384
+    Wait 16384
+    Wait 16384
+    Wait 16384
+
+    Wait 12241
+
+    ; Trigger = 1
+    ld a, $87
+    ldh [rNR14], a
+
+    Nops 5
+
+    ldh a, [rNR52]
+    cp $f0
+
+    jp nz, TestFail
+    jp TestSuccess
