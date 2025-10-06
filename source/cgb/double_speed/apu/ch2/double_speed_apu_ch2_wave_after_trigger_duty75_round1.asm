db377f8d608b8b08e7ed5d83266b4a94f41c6092
Docheinstein <dottorestefano97@gmail.com>
2025-10-06 22:17:02 +0200

    Add tests for output being 0 for CH1 and CH2 for the first duty step

---
+++ source/cgb/double_speed/apu/ch2/double_speed_apu_ch2_wave_after_trigger_duty75_round1.asm
@@ -0,0 +1,45 @@
+INCLUDE "all.inc"
+
+; Check that channel outputs 0 for the first duty step when it is turned on.
+
+EntryPoint:
+    ld hl, rPCM12
+
+    xor a
+    ldh [rDIV], a
+
+    DisableAPU
+
+    ; Prepare speed switch
+    ld a, $01
+    ldh [rKEY1], a
+
+    ; Switch to double speed
+    stop
+
+    EnableAPU
+
+    ; Duty Cycle = 75%
+    ld a, $C0
+    ldh [rNR21], a
+
+    ; Initial volume = F
+    ld a, $F0
+    ldh [rNR22], a
+
+    ; Period = 1
+    ld a, $FF
+    ldh [rNR23], a
+
+    ; Trigger = 1
+    ; Length enable = 1
+    ld a, $87
+    ldh [rNR24], a
+
+    Nops 3
+
+    ld a, [hl]
+    cp $00
+
+    jp nz, TestFail
+    jp TestSuccess
