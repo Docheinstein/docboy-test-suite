b9bdf1412ff350e82034da87f795791e0c3d0b16
Docheinstein <dottorestefano97@gmail.com>
2025-10-06 18:30:59 +0200

    Add tests for interrupt raised/not raised for transition between HBlank and OAM scan

---
+++ source/dmg/ppu/hblank_to_oam_clear_last_stat_irq_mode0.asm
@@ -0,0 +1,63 @@
+INCLUDE "all.inc"
+
+; Check if STAT HBlank interrupt is raised for the transition between HBlank and OAM Scan.
+
+EntryPoint:
+    DisablePPU
+    EnablePPU
+
+    ; Go to line 42
+    Wait 42 * 114
+
+    ; Skip OAM Scan
+    Nops 20
+
+    ; Reset IF
+    xor a
+    ldh [rIF], a
+
+    ; Enable STAT interrupt
+    ld a, IEF_STAT
+    ldh [rIE], a
+
+    ; Enable HBLANK and STAT interrupt
+    ld a, STATF_MODE00
+    ldh [rSTAT], a
+
+    ; Enable interrupts
+    ei
+
+    ld hl, FirstInterrupt
+
+    ld de, $00
+    ld bc, $00
+
+Loop1:
+    inc de
+    jp Loop1
+
+    jp TestFail
+
+FirstInterrupt:
+    ld hl, SecondInterrupt
+    ei
+
+Loop2:
+    inc bc
+    jp Loop2
+
+    jp TestFail
+
+SecondInterrupt:
+    ld a, c
+    cp $12
+    jp nz, TestFail
+
+    ld a, e
+    cp $03
+    jp nz, TestFail
+
+    jp TestSuccess
+
+SECTION "STAT handler", ROM0[$48]
+    jp hl
---
+++ source/dmg/ppu/hblank_to_oam_clear_last_stat_irq_mode0_lyc_eq_ly_prev.asm
@@ -0,0 +1,67 @@
+INCLUDE "all.inc"
+
+; Check if STAT HBlank interrupt is raised for the transition between HBlank and OAM Scan.
+
+EntryPoint:
+    ld a, 42
+    ld [rLYC], a
+
+    DisablePPU
+    EnablePPU
+
+    ; Go to line 42
+    Wait 42 * 114
+
+    ; Skip OAM Scan
+    Nops 20
+
+    ; Reset IF
+    xor a
+    ldh [rIF], a
+
+    ; Enable STAT interrupt
+    ld a, IEF_STAT
+    ldh [rIE], a
+
+    ; Enable HBLANK and LYC interrupt
+    ld a, STATF_MODE00 | STATF_LYC
+    ldh [rSTAT], a
+
+    ; Enable interrupts
+    ei
+
+    ld hl, FirstInterrupt
+
+    ld de, $00
+    ld bc, $00
+
+Loop1:
+    inc de
+    jp Loop1
+
+    jp TestFail
+
+FirstInterrupt:
+    ld hl, SecondInterrupt
+    ei
+
+Loop2:
+    inc bc
+    jp Loop2
+
+    jp TestFail
+
+SecondInterrupt:
+    ld a, c
+    cp $29
+    jp nz, TestFail
+
+    ld a, e
+    cp $00
+    jp nz, TestFail
+
+    jp TestSuccess
+
+
+SECTION "STAT handler", ROM0[$48]
+    jp hl
