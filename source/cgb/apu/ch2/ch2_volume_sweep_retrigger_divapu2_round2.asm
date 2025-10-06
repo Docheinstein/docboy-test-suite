11fa309e9e3a96f1d5869adc02493bcf6dfffae4
Docheinstein <dottorestefano97@gmail.com>
2025-10-06 23:02:28 +0200

    Add tests testing if retrigger affects CH2 volume sweep with various DIV-APU

---
+++ source/cgb/apu/ch2/ch2_volume_sweep_retrigger_divapu2_round2.asm
@@ -0,0 +1,43 @@
+INCLUDE "all.inc"
+
+; Check if retriggering affects volume sweep with various DIV-APU values..
+
+EntryPoint:
+    xor a
+    ldh [rDIV], a
+
+    DisableAPU
+    EnableAPU
+
+    ; Duty = 75 %
+    ld a, $C0
+    ldh [rNR21], a
+
+    ; Initial volume = F
+    ; Direction = 0 (decrease)
+    ; Pace = 1
+    ld a, $F1
+    ldh [rNR22], a
+
+    ; Period = 1
+    ld a, $FF
+    ldh [rNR23], a
+
+    ; Trigger = 1
+    ld a, $87
+    ldh [rNR24], a
+
+    Wait 8192
+    Wait 2048 * 5
+
+    ; Trigger = 1
+    ld a, $87
+    ldh [rNR24], a
+
+    Wait 14288
+
+    ldh a, [rPCM12]
+    cp $e0
+
+    jp nz, TestFail
+    jp TestSuccess
