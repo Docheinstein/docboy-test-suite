725f0210d891f9f24609745c0b506f605948948d
Docheinstein <dottorestefano97@gmail.com>
2025-11-05 00:35:10 +0100

    Add tests for DMG mode depending on CGB flag

---
+++ source/cgb_dmg_mode/ppu/rendering/palettes_cgb_flag_81_checksum_01.asm
@@ -0,0 +1,63 @@
+;! TITLE=ABSORPTION
+;! OLD_LICENSE=1
+
+INCLUDE "hardware.inc"
+
+SECTION "Header", ROM0[$100]
+    nop
+	jp EntryPoint
+
+	ds $143 - @, 0
+	db $81
+	ds $150 - @, 0
+
+SECTION "Home", ROM0[$150]
+
+INCLUDE "apu.inc"
+INCLUDE "cksum.inc"
+INCLUDE "debug.inc"
+INCLUDE "delay.inc"
+INCLUDE "halt.inc"
+INCLUDE "memory.inc"
+INCLUDE "ppu.inc"
+INCLUDE "print.inc"
+INCLUDE "test.inc"
+
+; Not in DMG mode if CGB flag is 81.
+
+EntryPoint:
+    DisablePPU
+
+    ; Write BG palette
+    ld a, %11100100
+    ldh [rBGP], a
+
+    ; Reset VRAM0
+    Memset $8000, $00, $2000
+
+    ; Set VRAM0 data
+    Memcpy $8800, BackgroundVram0TileData, BackgroundVram0TileDataEnd - BackgroundVram0TileData
+    Memcpy $9800, BackgroundVram0TileMapData, BackgroundVram0TileMapDataEnd - BackgroundVram0TileMapData
+
+    ; Enable PPU
+    EnablePPU
+
+    HaltForever
+
+BackgroundVram0TileData:
+    db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    db $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00
+    db $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+BackgroundVram0TileDataEnd:
+
+BackgroundVram0TileMapData:
+    db $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80
+    db $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80
+    db $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81
+    db $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81
+    db $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82
+    db $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82
+    db $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83
+    db $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83
+BackgroundVram0TileMapDataEnd:
---
+++ source/cgb_dmg_mode/ppu/rendering/palettes_cgb_flag_c0_checksum_01.asm
@@ -0,0 +1,44 @@
+;! TITLE=ABSORPTION
+;! OLD_LICENSE=1
+;! COLOR_ONLY=1
+
+INCLUDE "all.inc"
+
+; Not in DMG mode if CGB flag is C0.
+
+EntryPoint:
+    DisablePPU
+
+    ; Write BG palette
+    ld a, %11100100
+    ldh [rBGP], a
+
+    ; Reset VRAM0
+    Memset $8000, $00, $2000
+
+    ; Set VRAM0 data
+    Memcpy $8800, BackgroundVram0TileData, BackgroundVram0TileDataEnd - BackgroundVram0TileData
+    Memcpy $9800, BackgroundVram0TileMapData, BackgroundVram0TileMapDataEnd - BackgroundVram0TileMapData
+
+    ; Enable PPU
+    EnablePPU
+
+    HaltForever
+
+BackgroundVram0TileData:
+    db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    db $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00
+    db $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+BackgroundVram0TileDataEnd:
+
+BackgroundVram0TileMapData:
+    db $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80
+    db $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80, $80
+    db $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81
+    db $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81, $81
+    db $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82
+    db $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82, $82
+    db $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83
+    db $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83, $83
+BackgroundVram0TileMapDataEnd:
