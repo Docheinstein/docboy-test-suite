fdc7b1c5b80f35feaffe8272101f38f13f301249
Docheinstein <dottorestefano97@gmail.com>
2025-11-08 19:13:20 +0100

    Add tests for check that OPRI is honored in DMG mode

---
+++ source/cgb_dmg_mode/ppu/visual/obj_priority_alt.asm
@@ -0,0 +1,40 @@
+INCLUDE "all.inc"
+
+; Check that OBJ priority in DMG mode depends on the x coordinate first rather than on OAM address.
+
+EntryPoint:
+    DisablePPU
+
+    ; Set VRAM0 data
+    Memcpy $8800, BackgroundVram0TileData, BackgroundVram0TileDataEnd - BackgroundVram0TileData
+
+    ; Set OAM Data
+    Memset $fe00, $00, 160
+    Memcpy $fe00, OamData, OamDataEnd - OamData
+
+    ; Write OBJ palette
+    ld a, %11100100
+    ldh [rOBP0], a
+    ldh [rOBP1], a
+
+    EnablePPU_WithSprites
+
+    HaltForever
+
+BackgroundVram0TileData:
+    db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00
+    db $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00
+    db $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff, $00, $ff
+    db $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff, $ff
+BackgroundVram0TileDataEnd:
+
+OamData:
+    ; OBJ 1
+    db $10, 28, $81, $00
+
+    ; OBJ 2
+    db $10, 32, $82, $00
+
+    ; OBJ 3
+    db $10, 32, $83, $00
+OamDataEnd:
