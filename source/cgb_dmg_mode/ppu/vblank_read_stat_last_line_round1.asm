3189b4cb6e03d1d65c6a7bc4da466efb0a0b97cb
Docheinstein <dottorestefano97@gmail.com>
2025-11-15 19:24:23 +0100

    Add tests for last scanline vblank stat glitch

---
+++ source/cgb_dmg_mode/ppu/vblank_read_stat_last_line_round1.asm
@@ -0,0 +1,25 @@
+INCLUDE "all.inc"
+
+; Differently from DMG, the last scanline of VBLANK STAT goes straight from VBLANK to OAM SCAN (instead of passin through HBLANK).
+
+EntryPoint:
+    ; Reset STAT
+    xor a
+    ldh [rSTAT], a
+
+    ; Reset LYC
+    ld a, $10
+    ldh [rLYC], a
+
+    ; Reset PPU
+    DisablePPU
+    EnablePPU
+
+    Wait 114 * 153
+    Wait 109
+
+    ldh a, [rSTAT]
+    cp $81
+
+    jp nz, TestFail
+    jp TestSuccess
---
+++ source/cgb_dmg_mode/ppu/vblank_read_stat_last_line_round2.asm
@@ -0,0 +1,25 @@
+INCLUDE "all.inc"
+
+; Differently from DMG, the last scanline of VBLANK STAT goes straight from VBLANK to OAM SCAN (instead of passin through HBLANK).
+
+EntryPoint:
+    ; Reset STAT
+    xor a
+    ldh [rSTAT], a
+
+    ; Reset LYC
+    ld a, $10
+    ldh [rLYC], a
+
+    ; Reset PPU
+    DisablePPU
+    EnablePPU
+
+    Wait 114 * 153
+    Wait 110
+
+    ldh a, [rSTAT]
+    cp $80
+
+    jp nz, TestFail
+    jp TestSuccess
