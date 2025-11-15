274171336669e9c29ca494d9b00636464d9501cc
Docheinstein <dottorestefano97@gmail.com>
2025-11-15 19:24:11 +0100

    Add test for stat_write_bug on DMG mode

---
+++ source/cgb_dmg_mode/stat_write_bug/stat_write_bug_hblank.asm
@@ -0,0 +1,28 @@
+INCLUDE "all.inc"
+
+; STAT write bug does not happen even in DMG mode.
+
+EntryPoint:
+    DisablePPU
+
+    ; Write SCX=0
+    ld a, $00
+    ldh [rSCX], a
+
+    EnablePPU
+
+    Wait 114
+
+    xor a
+    ldh [rIF], a
+
+    Nops 60
+
+    ldh [rSTAT], a
+
+    ldh a, [rIF]
+    DumpRegisters
+
+    jp nz, TestFail
+
+    jp TestSuccess
