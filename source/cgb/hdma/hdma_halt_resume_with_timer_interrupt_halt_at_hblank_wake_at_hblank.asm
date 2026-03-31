410e632d9fbc29463a87c2edceda7cce9b84296a
Docheinstein <dottorestefano97@gmail.com>
2026-03-31 23:10:16 +0200

    Add tests for HDMA wake up from HALT

---
+++ source/cgb/hdma/hdma_halt_resume_with_timer_interrupt_halt_at_hblank_wake_at_hblank.asm
@@ -0,0 +1,100 @@
+INCLUDE "all.inc"
+
+; Perform a HDMA (HBlank) transfer, HALT, and wake up with Timer Interrupt.
+; Check how many chunks are transferred.
+; Event             | Mode
+; -----------------------------
+; HALT              | HBlank
+; Wake up from HALT | HBLank
+; LCD OFF           | HBlank
+
+EntryPoint:
+    DisablePPU
+
+    ; Set SCX=0
+    ld a, $00
+    ldh [rSCX], a
+
+    ; Set VRAM data
+    Memset $8000, $ab, $80
+
+    ; Copy data to WRAM2
+    Memcpy $D000, VramData, VramDataEnd - VramData
+
+    ; Source address = D000
+    ld a, $D0
+    ldh [rHDMA1], a
+
+    ld a, $00
+    ldh [rHDMA2], a
+
+    ; Dest address = 8000
+    ld a, $80
+    ldh [rHDMA3], a
+
+    ld a, $00
+    ldh [rHDMA4], a
+
+    ; Enable PPU
+    EnablePPU
+
+    ; Skip glitched line 0
+    Nops 114
+
+    ; Bit 7 = 1 (HBlank)
+    ; Length = 64 bytes / $10 - 1 => 3
+    ld a, $83
+    ldh [rHDMA5], a
+
+    ; Enable Timer interrupt
+    ld a, IEF_TIMER
+    ldh [rIE], a
+
+    ; Set TIMA to FF
+    ld a, $E0
+    ldh [rTIMA], a
+
+    ; Reset DIV
+    ldh [rDIV], a
+
+    ; Enable timer at 65536 Hz
+    ld a, TACF_START | TACF_65KHZ
+    ldh [rTAC], a
+
+    Nops 60
+
+    halt
+
+    ; Disable the PPU again
+    DisablePPU
+
+    Memcmp $8000, ExpectedVramData, ExpectedVramDataEnd - ExpectedVramData
+    jp nz, TestFail
+
+    jp TestSuccess
+
+VramData:
+    db $00, $11, $22, $33, $44, $55, $66, $77
+    db $88, $99, $aa, $bb, $cc, $dd, $ee, $ff
+    db $11, $22, $33, $44, $55, $66, $77, $88
+    db $99, $aa, $bb, $cc, $dd, $ee, $ff, $00
+    db $22, $33, $44, $55, $66, $77, $88, $99
+    db $aa, $bb, $cc, $dd, $ee, $ff, $00, $11
+    db $33, $44, $55, $66, $77, $88, $99, $aa
+    db $bb, $cc, $dd, $ee, $ff, $00, $11, $22
+
+VramDataEnd:
+
+
+ExpectedVramData:
+    db $00, $11, $22, $33, $44, $55, $66, $77
+    db $88, $99, $aa, $bb, $cc, $dd, $ee, $ff
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+    db $ab, $ab, $ab, $ab, $ab, $ab, $ab, $ab
+ExpectedVramDataEnd:
